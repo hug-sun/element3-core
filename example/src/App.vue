9086d04abe500fe03dd3aa8845d04908f8930b37
cuixiaorui <cui_xiaorui@126.com>
2021-05-13 11:14:09 +0800

    chore: add example for run

---
+++ example/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    hello element3-core
+
+    <div>
+      <Switch></Switch>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Switch } from "../../dist/element3-core.esm.js";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Switch,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
