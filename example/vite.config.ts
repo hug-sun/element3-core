a7d7925b92558065b1f4fe2aac8e01a80b1650e2
cuixiaorui <cui_xiaorui@126.com>
2021-05-17 19:12:38 +0800

    feat: import rollup-plugin-element3-webgl

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import webglLoader from "./webgl-loader/rollup-plugin-webgl-loader.js";
+import element3Webgl from "rollup-plugin-element3-webgl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), webglLoader()],
+  plugins: [vue(), element3Webgl()],
 });
