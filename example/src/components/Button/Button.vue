38f93c09ac4d152a16a835a9ddc3a0516b0225ce
cuixiaorui <cui_xiaorui@126.com>
2021-05-19 19:41:51 +0800

    chore: update element3-webgl version

--- example/src/components/Button/Button.vue
+++ example/src/components/Button/Button.vue
@@ -1,8 +1,38 @@
 <template>
   <div>
-    <E3Button class="EButton" v-model="enter" style="position:relative;border-radius:8px;">
-      <div :style="{transform:'translateY('+(enter?2:0)+'px)'}" style="font-size:24px;position:absolute;width:100%;text-align:center;line-height:60px;color:rgba(0,0,0,.6);text-shadow:0px 2px 1px #bbb;z-index:2;">按钮</div>
-      <draw :iTime="3.14159/8" :m1="m1" :m2="m2" :r="r" :g="g" :b="b" :style="enter?buttonEnterStyle:buttonStyle" width="100" height="60" :iWidth="buttonWidth" :iHeight="buttonHeight"></draw>
+    <E3Button
+      class="EButton"
+      v-model="enter"
+      style="position: relative; border-radius: 8px"
+    >
+      <div
+        :style="{ transform: 'translateY(' + (enter ? 2 : 0) + 'px)' }"
+        style="
+          font-size: 24px;
+          position: absolute;
+          width: 100%;
+          text-align: center;
+          line-height: 60px;
+          color: rgba(0, 0, 0, 0.6);
+          text-shadow: 0px 2px 1px #bbb;
+          z-index: 2;
+        "
+      >
+        按钮
+      </div>
+      <draw
+        :iTime="3.14159 / 8"
+        :m1="m1"
+        :m2="m2"
+        :r="r"
+        :g="g"
+        :b="b"
+        :style="enter ? buttonEnterStyle : buttonStyle"
+        width="100"
+        height="60"
+        :iWidth="buttonWidth"
+        :iHeight="buttonHeight"
+      ></draw>
     </E3Button>
   </div>
 </template>
@@ -16,19 +46,19 @@ export default {
     E3Button,
     draw,
   },
-  props:{
-    r:{
+  props: {
+    r: {
       type: Number,
-      default: .5
+      default: 0.5,
     },
-    g:{
+    g: {
       type: Number,
-      default: .5
+      default: 0.5,
     },
-    b:{
+    b: {
       type: Number,
-      default: .5
-    }
+      default: 0.5,
+    },
   },
   setup(props) {
     const enter = ref(false);
@@ -36,23 +66,23 @@ export default {
     const buttonWidth = ref(60);
     const buttonHeight = ref(100);
 
-    const buttonStyle ={
-      width:"100px",
-      height:"60px",
-      boxShadow:"0px 3px 4px #000",
-      borderRadius:"6px",
-      transform:"translateY(0px)"
-    }
-    const buttonEnterStyle ={
-      width:"100px",
-      height:"60px",
-      boxShadow:"0px 1px 4px #000",
-      borderRadius:"6px",
-      transform:"translateY(2px)"
-    }
+    const buttonStyle = {
+      width: "100px",
+      height: "60px",
+      boxShadow: "0px 3px 4px #000",
+      borderRadius: "6px",
+      transform: "translateY(0px)",
+    };
+    const buttonEnterStyle = {
+      width: "100px",
+      height: "60px",
+      boxShadow: "0px 1px 4px #000",
+      borderRadius: "6px",
+      transform: "translateY(2px)",
+    };
 
     const m1 = ref(75);
-    const m2 =ref(40);
+    const m2 = ref(40);
 
     return {
       buttonWidth,
@@ -68,8 +98,7 @@ export default {
 </script>
 
 <style>
-  .EButton:focus
-  {
-    outline: none;
-  }
+.EButton:focus {
+  outline: none;
+}
 </style>
