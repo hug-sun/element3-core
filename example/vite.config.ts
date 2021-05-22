d7eceafb9cbe17de2c702845f171ed1c8d66f76d
cuixiaorui <cui_xiaorui@126.com>
2021-05-22 17:12:01 +0800

    docs: update webgl demo

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,5 +4,6 @@ import element3Webgl from "rollup-plugin-element3-webgl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/webgl", // TODO 开发环境是 / 生产环境是 /webgl
   plugins: [vue(), element3Webgl()],
 });
